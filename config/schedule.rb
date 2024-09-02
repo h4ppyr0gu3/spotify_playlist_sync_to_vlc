6091aa3f0d268bb6552cde3198e91fed9dd44384
david <david@h4ppyr0gu3.one>
2024-09-02 10:55:37 +0200

    fix schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 #
-every 1.minute do
+every 1.day, at: '8:00 pm' do
   command ". $HOME/.asdf/asdf.sh && /home/david/.asdf/shims/ruby /usr/local/bin/spotify_to_vlc -u 31whpm5krwcqauctazr2q6cyuwbu -s 7d258f88c8504671afae3daa0b4c1dc2 -i be46642fac0349e5be8701a3334f531b -p 3PkpP9NiY375g8wEx6ku7r,5SpDRqWjaGu7akzXJWSARu,4LlKG5tStLLUEQkOjvV9fC,2rX83BkoF3koFmedoUDs3J,7AMTTWYINFmWSwVRCmzo7G,134jLTtO7IgczbqjVEvSEB,66Qen9GZ1uDHYFfVzWQJzc,1sRiEecMJj2qbhPj805JkL -o /home/david/Music -v >> $HOME/.spotify_cron_log 2>&1"
 end
